2eec8abb686dbc9cab3c7465117149c82c7e088d
Md. Mehedi Hasan Riyad <mdmehedihasan-2017615008@cs.du.ac.bd>
2023-07-23 18:03:51 +0600

    Delete src directory

--- src/main/java/com/example/restapp/repository/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.restapp.repository;
-
-import com.example.restapp.model.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
